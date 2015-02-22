412e129cda1bf9ebbb391c0bf5dd83030d85d9f2
=arunkumar3 <arunkumar.jalanila@gmail.com>
2015-02-22 14:42:16 -0700

    add color and shape to the plot using geom_plot and ggplot

--- ggplot.R
+++ ggplot.R
@@ -22,3 +22,19 @@ summary(myplot) #summary of the ggplot
 #creating a geometry using geom_point along with ggplot then it will create a plot 
 
 myplot + geom_point()
+
+#creating a plot with adding color to each group using color attribute
+myplot <- ggplot(data=iris,aes(x=Sepal.Length,y=Sepal.Width,color =Species)) 
+myplot + geom_point(size=3)
+
+#creating a plot with adding shape to each group using shape attribute in geom point
+myplot <- ggplot(data=iris,aes(x=Sepal.Length,y=Sepal.Width,color =Species)) 
+myplot + geom_point(aes(shape =Species),size=5)
+
+
+#sampling thousand rows from diamonds data set and plotting using ggplot and geom_point
+d2 <- diamonds[sample(1:dim(diamonds)[1],1000),] #sampling 1000 rows from total number of rows of diamonds data set
+d2plot <- ggplot(data=d2,aes(x=carat,y=price,color=color))
+d2plot + geom_point(size =3)
+
+
